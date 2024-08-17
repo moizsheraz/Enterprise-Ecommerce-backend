743e7779b15302986909c2da2c4ec5fcd294c35d
moizsheraz <user146712@gmail.com>
2024-08-17 20:00:28 +0500

    init

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import express from 'express';
 import routes from './routes';
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
