4d26fc3378e15d97ea1c5d5fc90ddb14fb304dc4
moizsheraz <user146712@gmail.com>
2024-08-25 20:35:23 +0500

    feat:Test added

---
+++ e2e-tests/tests/auth.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from "@playwright/test";
+
+const UI_URL = "http://localhost:5173";
+
+test("should allow the user to sign in", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  // get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign in to your account" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("tes@gmail.com");
+  await page.locator("[name=password]").fill("bola party 5678");
+
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  await expect(page.getByText("Sign in Successful!")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+});
+
+test("should allow user to register", async ({ page }) => {
+  const testEmail = `test_register_${
+    Math.floor(Math.random() * 90000) + 10000
+  }@test.com`;
+  await page.goto(UI_URL);
+
+  await page.getByRole("link", { name: "Try for Free" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Sign up into your account" })
+  ).toBeVisible();
+
+  await page.locator("[name=firstName]").fill("test_firstName");
+  await page.locator("[name=lastName]").fill("test_lastName");
+  await page.locator("[name=email]").fill(testEmail);
+  await page.locator("[name=password]").fill("password123");
+  await page.locator("[name=confirmPassword]").fill("password123");
+
+  await page.getByRole("button", { name: "Sign up" }).click();
+
+  await expect(page.getByText("Registration successful")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+});
--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ const app = express();
 
 app.use(
   cors({
-    // origin: "http://localhost:5173", 
-    origin:"https://enterprise-ecommerce-frontend.vercel.app",
+    origin: "http://localhost:5173", 
+    // origin:"https://enterprise-ecommerce-frontend.vercel.app",
     credentials: true, 
   })
 );
